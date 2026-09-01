b1493eb19ebbe04819f961e7a4bd80a543d8b2ab
mcosta <1251998@isep.ipp.pt>
2026-09-01 12:27:26 +0100

    Initial commit

---
+++ src/main/java/com/recipes/RecipesAppApplication.java
@@ -0,0 +1,16 @@
+package com.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the Recipes App Spring Boot application.
+ */
+@SpringBootApplication
+public class RecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesAppApplication.class, args);
+	}
+
+}
